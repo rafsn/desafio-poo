fbcf8eaf265ada8e7578e9c5c740bedc31f04b6e
whazys <santos_rafa1973@hotmail.com>
2022-10-08 21:53:03 -0300

    Iniciando POO - Ifood

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.dio.desafio.dominio.Curso;
+import br.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso Javascript");
+        curso2.setDescricao("descrição curso Javascript");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
